abb251c31325d8e554f0fa82eb5451cec3ef1035
João Victor <jvictorsantos852@gmail.com>
2020-02-07 06:01:11 -0300

    persistor and store configured

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import React from 'react';
 import '~/config/ReactotronConfig';
 
 import { Provider } from 'react-redux';
-import store from './store';
+import { store, persistor } from '~/store';
 
 import Routes from '~/routes';
 
 const App = () => (
   <Provider store={store}>
-    <Routes />
+    <PersistGate persistor={persistor}>
+      <Routes />
+    </PersistGate>
   </Provider>
 );
 
