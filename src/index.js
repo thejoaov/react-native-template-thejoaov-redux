8c99fb08645c826b5b388ec9bc8b71b75a78e90f
João Victor <jvictorsantos852@gmail.com>
2020-02-07 06:18:46 -0300

    fixed redux-persist

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import '~/config/ReactotronConfig';
 
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { store, persistor } from '~/store';
 
