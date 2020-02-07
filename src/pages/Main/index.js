8e356487a93bcd2945b7b83c6654389ee310d4d2
João Victor <jvictorsantos852@gmail.com>
2020-02-07 06:50:35 -0300

    fixed page index

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text } from 'react-native';
 import api from '~/services/api';
 
@@ -13,21 +13,27 @@ import {
 export default function Main() {
   const [value, onChangeText] = useState('');
   const [user, setUser] = useState([]);
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    user.avatar_url ? setVisible(true) : setVisible(false);
+  }, [user]);
 
   async function searchUser() {
-    const { data } = await api.get(`/user/${value}`);
+    const { data } = await api.get(`/users/${value}`);
     setUser(data);
   }
 
   return (
     <Container>
       <HelloWorld>Hello World!</HelloWorld>
-      {user ? (
+      {!visible ? (
         <>
           <Text>Type a Github username</Text>
           <GithubUserInput
             onChangeText={text => onChangeText(text)}
             value={value}
+            autoCapitalize="none"
           />
           <GithubUserButton onPress={() => searchUser()}>
             <Text>Search!</Text>
@@ -37,6 +43,9 @@ export default function Main() {
         <>
           <GithubUserPhoto source={{ uri: user.avatar_url }} />
           <Text>{user.name}</Text>
+          <GithubUserButton onPress={() => setVisible(false)}>
+            <Text>Search Again!</Text>
+          </GithubUserButton>
         </>
       )}
     </Container>
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -4,7 +4,7 @@ export const Container = styled.SafeAreaView`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: #2196f3;
+  background-color: #e5e5e5;
 `;
 
 export const HelloWorld = styled.Text`
@@ -14,6 +14,12 @@ export const HelloWorld = styled.Text`
 export const GithubUserInput = styled.TextInput`
   height: 50px;
   width: 280px;
+  background-color: #fff;
+  border-radius: 5px;
+  text-align: center;
+  margin: 20px 0;
+  border: 1px;
+  border-color: #34b6ff;
 `;
 
 export const GithubUserButton = styled.TouchableOpacity`
@@ -22,10 +28,14 @@ export const GithubUserButton = styled.TouchableOpacity`
   align-items: center;
   justify-content: center;
   text-align: center;
+  background-color: #34b6ff;
+  border-radius: 5px;
+  margin: 20px 0;
 `;
 
 export const GithubUserPhoto = styled.Image`
   width: 100px;
   height: 100px;
   border-radius: 50px;
+  margin: 30px 0;
 `;
