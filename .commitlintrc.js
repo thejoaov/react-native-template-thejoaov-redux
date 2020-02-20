33ea21fe19487eb232b7e5c0d7dd116ec010525b
João Victor <jvictorsantos852@gmail.com>
2020-02-19 23:58:45 -0300

    commit lint rules

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,3 +1,13 @@
+/**
+ * Commit lint
+ * Read more on
+ * https://github.com/conventional-changelog/commitlint
+ *
+ * The rule used is "@commitlint/config-angular"
+ * Read more about it on
+ * https://github.com/conventional-changelog/commitlint/tree/master/@commitlint/config-angular
+ */
+
 module.exports = {
   extends: ['@commitlint/config-angular'],
 };
